30d0ffa684e3ac3f84e4d7ab7dec5943834bae30
XYCccccc <43713161+XYCccccc@users.noreply.github.com>
2019-04-12 09:46:53 +0800

    Add files via upload

---
+++ js/DOM.js
@@ -0,0 +1,10 @@
+function init () {
+  var divtag = document.getElementById('agou')
+  var divaaa = document.getElementById('aaa')
+  var divsign = document.getElementById('amao')
+  divtag.setAttribute ('style', 'height: 250px; width: 250px; background-color: red; margin: auto')
+  divaaa.setAttribute ('style', 'height: 250px; width: 250px; background-color: yellow; margin: auto')
+  divsign.setAttribute ('style', 'height: 250px; width: 250px; background-color: green; margin: auto')
+}
+
+window.onload = init
