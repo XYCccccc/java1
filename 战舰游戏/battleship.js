6e3c7d464edfdc949831a91a832c47dae4dac27d
XYCccccc <43713161+XYCccccc@users.noreply.github.com>
2019-05-16 11:03:02 +0800

    Add files via upload

--- 战舰游戏/battleship.js
+++ 战舰游戏/battleship.js
@@ -1,15 +1,15 @@
 var view ={
   displayarea: function(msg) {
     var area  = document.getElementById("area");
-    area.innerHTML = msg
+    area.innerHTML = msg;
   },
   displayHit: function(location) {
     var cell = document.getElementById(location);
-    cell.setAttribute("class" , "hit")
+    cell.setAttribute("class" , "hit");
   },
   displayMiss: function(location) {
     var cell = document.getElementById(location);
-    cell.setAttribute("class" , "miss")
+    cell.setAttribute("class" , "miss");
   }
 }
 
@@ -27,21 +27,23 @@ var model = {
       var index = ship.locations.indexOf(guess);
       if (index >= 0) { ship.hits[index] = "hit";
       view.displayHit(guess);
-      view.displayarea("HIT!");
+      view.displayarea("击中!");
       if (this.isSunk(ship)) {
-        view.displayarea("You sank my battleship!");
+        view.displayarea("你击了沉我的战舰!");
         this.shipsSunk++;
       }
       return true;
     }
   }
   view.displayMiss(guess);
-  view.displayarea("You missed.");
+  view.displayarea("没打中,请继续猜测。");
   return false;
 },
 isSunk: function(ship) {
   for (var i = 0; i < this.shipLength; i++)  {
     if (ship.hits[i] !== "hit") {
+      count++;
+      Math.cell(count / hits.length);
       return false;
     }
   }
@@ -90,41 +92,51 @@ collision: function(locations) {
 }
 var controller = {
   guesses: 0,
-  processGuess: function(guess) {
-    var location = parseGuess(guess);
-    if (location) {
-      this.guesses ++;
+  processGuess: function(e) {
+    var paramType = typeof e;
+    var location
+    if (paramType === 'object') {
+      location = e.target.id
+    } else {
+      location = parseGuess(e)
+    }
+    if(location){
+      controller.guess++;
       var hit = model.fire(location);
-      if (hit && model.shipsSunk === model.numShips) {
-        view.displayarea("你击沉了我们所有战舰, 经过 " + this.guesses + "次猜测");
+      if (hit&& model.shipsSunk === model.numShips) {
+        view.displayarea("你击沉了我的战舰,共计猜测" + controller.guesses + "次")
       }
     }
   }
 }
-function parseGuess(guess) {
-  var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
-   if (guess === null || guess.length !== 2) {
-     alert("请输出符合规定的输入");
-    } else {
-      firstChar = guess.charAt(0);
-      var row = alphabet.indexOf(firstChar);
+
+function parseGuess(guess) {   var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+   if (guess === null || guess.length !== 2) { 
+     alert("请输出符合规定的输入");   
+    } else { 
+      firstChar = guess.charAt(0); 
+      var row = alphabet.indexOf(firstChar); 
       var column = guess.charAt(1);
+      if (isNaN(row) || isNaN(column)) { 
+        alert("请输入范围内的数字"); 
+      } else if (row < 0 || row >= model.boardSize ||   column < 0 || column >= model.boardSize) {
+         alert("输入内容不在范围内"); 
+        } else { 
+          return row + column; 
+        }
+      }   
+      return null; 
+    }
 
-
-   if (isNaN(row) || isNaN(column)) {
-     alert("请输入范围内的数字");
-    } else if (row < 0 || row >= model.boardSize ||column < 0 || column >= model.boardSize) {
-        alert("输入内容不在范围内"); 
-      } else {
-        return row + column;
-      }
-  }
-  return null;
-}
 function init () {
   var fireButton = document.getElementById("fireButton");
+  var mouse = document.getElementsByTagName('td');
+  for (var i = 0; i<mouse.length; i++) {
+    mouse[i].onclick = controller.processGuess;
+  }
+
   fireButton.onclick = handleFireButton;
-  var guessInput = document.getElementById("guessInput");
+  var guessInput = document .getElementById("guessInput");
   guessInput.onkeypress = handleKeyPress;
   model.generateShipLocations();
 }
@@ -140,10 +152,18 @@ function handleFireButton() {
 
 function handleKeyPress(e) {
   var fireButton = document.getElementById("fireButton");
-  if (e.KeyCode === 13) {
+  if (e.keyCode === 13) {
     fireButton.click();
     return false;
   }
 }
 
+function test() {
+  var guess = guessInput.value;
+  controller.processGuess(guess);
 
+  guessInput.value = "";
+}
+
+
+view.displayarea("猜测战舰位置，选定并开炮")
