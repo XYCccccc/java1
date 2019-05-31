c83fa828702342f5ac066af8a64e20f4e5709225
XYCccccc <43713161+XYCccccc@users.noreply.github.com>
2019-05-31 09:51:15 +0800

    Add files via upload

---
+++ 信息1701 31 薛宇成/battleship.js
@@ -0,0 +1,184 @@
+var view ={
+  displayarea: function(msg) {
+    var area  = document.getElementById("area");
+    area.innerHTML = msg;
+  },
+  displayHit: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class" , "hit");
+  },
+  displayMiss: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class" , "miss");
+  }
+}
+
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipsSunk: 0,
+  shipLength: 3,
+  ships: [ { locations: ["0", "0", "0"], hits: ["", "", ""] },
+           { locations: ["0", "0", "0"], hits: ["", "", ""] },
+           { locations: ["0", "0", "0"], hits: ["", "", ""] } ],
+  fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      if (index >= 0) {
+      ship.hits[index] = "hit";
+      view.displayHit(guess);
+      view.displayarea('<h1>' + "击中!" + '</h1>');
+      if (this.isSunk(ship)) {
+        view.displayarea('<h1>' + "你击了沉我的战舰!" + '</h1>');
+        this.shipsSunk++;
+      }
+      return true;
+    }
+  }
+  view.displayMiss(guess);
+  view.displayarea('<h1>' + "没打中,请继续猜测。" + '</h1>');
+  return false;
+},
+isSunk: function(ship) {
+  var count = 0;
+  for(var i = 0; i < this.shipLength; i++) {
+    if(ship.hits[i] === "hit") {
+      count++
+      if(count === 1) {
+        for (var k = 0; k < this.shipLength; k++) {
+          var vcolor = document.getElementById(ship.locations[k])
+          view.displayHit(ship.locations[k])
+          vcolor.style.backgroundImage = "url('ship.png')"
+          var ship1 = document.getElementById(event.target.id)
+          ship1.style.backgroundImage = "url('baozha.png')"
+        }
+      } else {
+        for (var g = 0; g < this.shipLength; g++) {
+          var vcolor = document.getElementById(ship.locations[g])
+          vcolor.style.backgroundImage = "url('baozha.png')"
+        }
+      }
+    }
+    if(this.shipLength * 0.66 < count) {
+      return true;
+    }
+  }
+  return false
+},
+generateShipLocations: function () {
+  var locations;
+  for (var i = 0; i < this.numShips; i++) {
+    do {
+      locations = this.generateShip();
+    } while (this.collision(locations));
+    this.ships[i].locations = locations;
+  }
+ },
+generateShip: function() {
+  var direction = Math.floor(Math.random() * 2);
+  var row, col;
+  if (direction === 1) {
+    row = Math.floor(Math.random() * this.boardSize);
+    col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+  } else {
+    row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+    col = Math.floor(Math.random() * this.boardSize);
+  }
+  var newShipLocations = [];
+  for (var i = 0; i < this.shipLength; i++) {
+    if (direction === 1) {
+      newShipLocations.push(row + "" + (col + i));
+    } else { 
+      newShipLocations.push((row + i) + "" + col);
+  }
+ }
+  return newShipLocations;
+ },
+collision: function(locations) {
+  for (var i = 0; i < this.numShips; i++) {
+    var ship = model.ships[i];
+    for (var j = 0; j < locations.length; j++) {
+      if (ship.locations.indexOf(locations[j]) >= 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+ }
+}
+var controller = {
+  guesses: 0,
+  processGuess: function(e) {
+    var paramType = typeof e;
+    var location
+    if (paramType === 'object') {
+      location = e.target.id
+    }  else {
+      location = parseGuess(e)
+    }
+    if(location){
+      controller.guesses++;
+      var hit = model.fire(location);
+      if (hit&& model.shipsSunk === model.numShips) {
+        view.displayarea('<h1>' + "你击沉了我的战舰,共计猜测" + controller.guesses + "次" + '</h1>')
+      }
+    }
+  }
+}
+function parseGuess(guess) {
+   var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+   if (guess === null || guess.length !== 2) { 
+     alert("请输出符合规定的输入");   
+    } else { 
+      firstChar = guess.charAt(0); 
+      var row = alphabet.indexOf(firstChar); 
+      var column = guess.charAt(1);
+      if (isNaN(row) || isNaN(column)) { 
+        alert("请输入范围内的数字"); 
+      } else if (row < 0 || row >= model.boardSize ||   column < 0 || column >= model.boardSize) {
+         alert("输入内容不在范围内"); 
+        } else { 
+          return row + column; 
+        }
+      }   
+      return null; 
+    }
+
+function init () {
+  var fireButton = document.getElementById("fireButton");
+  var mouse = document.getElementsByTagName('td');
+  for (var i = 0; i<mouse.length; i++) {
+    mouse[i].onclick = controller.processGuess;
+  }
+  fireButton.onclick = handleFireButton;
+  var guessInput = document .getElementById("guessInput");
+  guessInput.onkeypress = handleKeyPress;
+  model.generateShipLocations();
+}
+window.onload = init;
+
+function handleFireButton() {
+  var guessInput = document.getElementById("guessInput");
+  var guess = guessInput.value;
+  controller.processGuess(guess);
+
+  guessInput.value = "";
+}
+
+function handleKeyPress(e) {
+  var fireButton = document.getElementById("fireButton");
+  if (e.keyCode === 13) {
+    fireButton.click();
+    return false;
+  }
+}
+
+function test() {
+  var guess = guessInput.value;
+  controller.processGuess(guess);
+
+  guessInput.value = "";
+}
+
+view.displayarea('<h1>' + "猜测战舰位置，选定并开炮" + '</h1>')
